64c7ad7e05b417ae8c4d37d579fcc480125708fc
ialcazar <israelalcazar@gmail.com>
2010-05-04 18:13:20 +0200

    Creados test automaticamente

---
+++ test/unit/com/farmerdev/quedada/TipoTests.groovy
@@ -0,0 +1,17 @@
+package com.farmerdev.quedada
+
+import grails.test.*
+
+class TipoTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
---
+++ test/unit/com/farmerdev/quedada/UbicacionTests.groovy
@@ -0,0 +1,17 @@
+package com.farmerdev.quedada
+
+import grails.test.*
+
+class UbicacionTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
